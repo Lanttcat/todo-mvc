433ec7cc22a88f4872db8c398f8f34cd2167a244
NikoManiac <niko.sky.lan@gmail.com>
2021-05-06 00:27:35 +0800

    [style] update style of clear complete

--- src/components/task-tools/__test__/task-tools.test.tsx
+++ src/components/task-tools/__test__/task-tools.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import TaskTools from '../index';
 import { render } from "../../../helper/test-helper";
 
 describe('TaskTools component', () => {
   test('should render tools', () => {
-    render(<TaskTools leftCount={3} onClearCompleted={() => {}} onFilter={() => {}} />);
+    render(<TaskTools leftCount={3} showClear={false} onClearCompleted={() => {}} onFilter={() => {}} />);
     const inputElement = screen.getByText(/3 items left/i);
     expect(inputElement).toBeInTheDocument();
   });
